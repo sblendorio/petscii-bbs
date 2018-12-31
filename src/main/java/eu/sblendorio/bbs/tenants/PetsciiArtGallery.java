b80c87d3f227e49ff7f079f5866ab1193350b429
francescosblendorio <francesco.sblendorio@jobrapido.com>
2018-12-31 18:33:02 +0100

    Show PETSCII art by array list

--- src/main/java/eu/sblendorio/bbs/tenants/PetsciiArtGallery.java
+++ src/main/java/eu/sblendorio/bbs/tenants/PetsciiArtGallery.java
@@ -1,17 +1,27 @@
 package eu.sblendorio.bbs.tenants;
 
 import eu.sblendorio.bbs.core.PetsciiThread;
+
 import static eu.sblendorio.bbs.core.Keys.UPPERCASE;
+import static eu.sblendorio.bbs.core.Keys.CASE_LOCK;
 
 public class PetsciiArtGallery extends PetsciiThread {
 
+    public static String[] pictures = {
+            "walkingdead",
+            "nightdriver",
+            "magicportal",
+            "baby-crying"
+    };
+
     @Override
     public void doLoop() throws Exception {
-        cls();
-        write(UPPERCASE);
-        writeRawFile("walkingdead"); resetInput(); readKey(); cls();
-        writeRawFile("nightdriver"); resetInput(); readKey(); cls();
-        writeRawFile("magicportal"); resetInput(); readKey(); cls();
-        writeRawFile("babycrying");  resetInput(); readKey(); cls();
+        write(UPPERCASE, CASE_LOCK);
+        for (String picture: pictures) {
+            cls();
+            writeRawFile(picture);
+            resetInput();
+            readKey();
+        }
     }
 }
