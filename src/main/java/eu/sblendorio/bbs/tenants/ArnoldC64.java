5ce6d7a3a3ad581d7e9ffef0c314bddd84b9c1f9
francesco.sblendorio <francesco.sblendorio@eidosmedia.com>
2019-07-05 17:44:43 +0200

    removed blank line@

--- src/main/java/eu/sblendorio/bbs/tenants/ArnoldC64.java
+++ src/main/java/eu/sblendorio/bbs/tenants/ArnoldC64.java
@@ -138,7 +138,6 @@ public class ArnoldC64 extends PetsciiThread {
         flush();
     }
 
-
     private void displayPost(int n) throws Exception {
         int i = 3;
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
