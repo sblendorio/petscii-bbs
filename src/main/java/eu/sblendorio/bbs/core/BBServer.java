9612782550456cec1fdfed1b702e62ec85689f46
francesco.sblendorio <francesco.sblendorio@eidosmedia.com>
2019-10-24 17:46:42 +0200

    Typo

--- src/main/java/eu/sblendorio/bbs/core/BBServer.java
+++ src/main/java/eu/sblendorio/bbs/core/BBServer.java
@@ -34,7 +34,7 @@ public class BBServer {
                 socket.setSoTimeout(timeout);
 
                 CbmInputOutput cbm = new CbmInputOutput(socket);
-                PetsciiThread thread = bbs.getDeclaredConstructor().gitnewInstance();
+                PetsciiThread thread = bbs.getDeclaredConstructor().newInstance();
                 thread.setSocket(socket);
                 thread.setCbmInputOutput(cbm);
                 thread.start();
