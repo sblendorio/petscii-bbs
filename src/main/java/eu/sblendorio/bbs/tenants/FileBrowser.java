e68fcc08f91f5c6944d8cfdd0bd6cb503353efe5
sblendorio <sblendorio@gmail.com>
2019-11-26 23:59:42 +0100

    List files

--- src/main/java/eu/sblendorio/bbs/tenants/FileBrowser.java
+++ src/main/java/eu/sblendorio/bbs/tenants/FileBrowser.java
@@ -9,23 +9,41 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-import static eu.sblendorio.bbs.core.Colors.WHITE;
+import static eu.sblendorio.bbs.core.Colors.*;
 import static eu.sblendorio.bbs.core.Keys.*;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.apache.commons.lang3.StringUtils.repeat;
 
 public class FileBrowser extends PetsciiThread {
 
     protected static final String SHARED_FOLDER = System.getProperty("user.home") + "/shared";
-
     private String currentDir = "/";
-
     private static final int ROWS = 10;
 
     @Override
     public void doLoop() throws Exception {
         write(WHITE, LOWERCASE, CASE_LOCK, HOME);
         cls();
-        box(0,3,38,24);
+        write(LOGO_BYTES);
+        write(BLUE);
+        box(0,5,38,24);
+        write(GREY3);
+        displayFiles();
+    }
+
+    private void displayFiles() {
+        File file = Paths.get(SHARED_FOLDER).toFile();
+        File[] files = file.listFiles();
+        int count = files.length;
+        gotoXY(0, 6); // INSERIRE IL CASO NUMERO DI FILE DISPARI
+        for (int i=0; i<count / 2; ++i) {
+            for (int j=0; j<1; ++j) write(RIGHT);
+            String entry = files[i].getName()+(files[i].isDirectory() ? "/" : EMPTY);
+            print(entry);
+            for (int j=0; j<20-(1+entry.length()+1); ++j) write(RIGHT);
+            entry = files[count/2 + i].getName()+(files[count/2 + i].isDirectory() ? "/" : EMPTY);
+            println(entry);
+        }
     }
 
     public void box(int x1, int y1, int x2, int y2) {
@@ -56,4 +74,19 @@ public class FileBrowser extends PetsciiThread {
         Arrays.stream(file.listFiles()).forEach(x -> System.out.println(x.getName() + (x.isDirectory() ? "/" : "")));
     }
 
+    private final byte[] LOGO_BYTES = {
+            32, 18, 31, -66, 32, 32, -110, 32, 32, 32, -97, -84, -94, -94, -84, -69,
+            -94, 32, 32, -94, -94, -69, 32, 32, -127, -84, -69, 32, 32, 32, 32, -69,
+            32, 32, -69, 13, 18, 31, -95, -84, -110, 32, 32, 18, 32, -84, -110, 32,
+            18, -97, -95, -84, -94, -95, -110, -95, 18, 32, -110, 32, 32, 18, 32, -94,
+            -110, -66, 32, 32, -127, -95, 18, -95, -110, 32, 32, 32, 32, -68, -69, 18,
+            -65, -110, 13, 18, 31, -95, -110, -95, 32, 32, 28, -94, -69, 32, 18, -97,
+            -95, -84, -110, -66, 18, -95, -110, -95, 18, 32, -110, 32, 32, 18, 32, -94,
+            -110, 32, 32, 32, 18, -127, -68, -94, -110, -65, -84, 18, -94, -110, -69, 32,
+            18, -65, -110, -69, 13, 31, -68, 18, 32, -110, -94, -94, 18, 28, -94, -94,
+            -110, 32, 18, -97, -95, -110, -95, 32, 18, -95, -110, -95, 18, 32, 32, -110,
+            -95, 18, 32, 32, -110, -95, 32, -127, -65, 18, -68, -110, -94, 18, -65, -110,
+            -68, -94, -66, 18, -65, -110, 32, -68, -69, 13, 32, 31, -68, 18, -94, -94,
+            -110, 13
+    };
 }
