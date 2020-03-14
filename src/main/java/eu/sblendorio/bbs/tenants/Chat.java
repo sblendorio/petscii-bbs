2949cef9159d070eab73ea04c0997a17f909d042
francesco.sblendorio <francesco.sblendorio@eidosmedia.com>
2020-03-14 16:57:36 +0100

    Clear chat user after exit

--- src/main/java/eu/sblendorio/bbs/tenants/Chat.java
+++ src/main/java/eu/sblendorio/bbs/tenants/Chat.java
@@ -12,6 +12,7 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.stream.Collectors;
 
@@ -50,66 +51,83 @@ public class Chat extends PetsciiThread {
 
     @Override
     public void doLoop() throws Exception {
-        write(Colors.GREY3, Keys.CLR, Keys.LOWERCASE, Keys.CASE_LOCK, Keys.HOME);
-        int status;
-        do {
-            print("Enter your name: ");
-            String name = readLine();
-            status = changeClientName(name);
-            if (status != 0) {
-                println("Error: name already used.");
-            }
-        } while (status != 0);
-
-        String command = null;
-        do {
-            write(Keys.CLR, Colors.YELLOW);
-            print("              BBS Chat 1.0");
-            write(Colors.WHITE); println(StringUtils.repeat(' ', 13 - defaultString(getClientName()).length()) + getClientName());
-            write(Colors.BLUE);
-            println("Commands");
-            write(Colors.LIGHT_BLUE); print("/users "); write(Colors.GREY2); println("        to list users");
-            write(Colors.LIGHT_BLUE); print("/to <person>"); write(Colors.GREY2); println("   to talk with someone");
-            write(Colors.LIGHT_BLUE); print("/nick <name>"); write(Colors.GREY2); println("   to change nick");
-            write(Colors.WHITE); print(".           "); write(Colors.GREY2); println("   to exit chat");
-            write(Colors.WHITE); print("<RETURN>"); write(Colors.GREY2); println("       to refresh screen");
-            println();
-            displayMessages();
-
-            String recipientName = Optional.ofNullable(recipient)
-                    .map(x -> getClients().get(x).getClientName())
-                    .orElse(null);
-            if (recipientName != null) {
-                write(Colors.YELLOW);
-                print("[" + recipientName + "]");
-            }
-
-            write(Colors.GREY3);
-            print(">");
-            command = readLine();
-            command = defaultString(command).trim();
-            if (StringUtils.isBlank(command)) {
-                continue;
-            } else if (command.matches("(?is)/to [a-zA-Z0-9]+")) {
-                Long candidateRecipient = getClientIdByName(command.replaceAll("\\s+", " ").substring(4));
-                if (candidateRecipient != null && candidateRecipient != getClientId()) {
-                    recipient = candidateRecipient;
+        try {
+            write(Colors.GREY3, Keys.CLR, Keys.LOWERCASE, Keys.CASE_LOCK, Keys.HOME);
+            int status;
+            do {
+                print("Enter your name: ");
+                String name = readLine();
+                status = changeClientName(name);
+                if (status != 0) {
+                    println("Error: name already used.");
                 }
-            } else if (command.matches("(?is)/nick [a-zA-Z0-9]+")) {
-                String newName = command.replaceAll("\\s+", " ").substring(6);
-                int res = changeClientName(newName);
-                if (res != 0) {
-                    println("Error: name already used. Press any key.");
-                    readKey();
-                }
-            } else if (command.equalsIgnoreCase("/users")) {
-                showUsers();
-            } else if (recipient != null) {
-                send(recipient, new Message(recipient, command));
-                send(getClientId(), new Message(recipient, command));
-            }
-        } while (!".".equals(command));
+            } while (status != 0);
 
+            String command = null;
+            do {
+                write(Keys.CLR, Colors.YELLOW);
+                print("              BBS Chat 1.0");
+                write(Colors.WHITE);
+                println(StringUtils.repeat(' ', 13 - defaultString(getClientName()).length()) + getClientName());
+                write(Colors.BLUE);
+                println("Commands");
+                write(Colors.LIGHT_BLUE);
+                print("/users ");
+                write(Colors.GREY2);
+                println("        to list users");
+                write(Colors.LIGHT_BLUE);
+                print("/to <person>");
+                write(Colors.GREY2);
+                println("   to talk with someone");
+                write(Colors.LIGHT_BLUE);
+                print("/nick <name>");
+                write(Colors.GREY2);
+                println("   to change nick");
+                write(Colors.WHITE);
+                print(".           ");
+                write(Colors.GREY2);
+                println("   to exit chat");
+                write(Colors.WHITE);
+                print("<RETURN>");
+                write(Colors.GREY2);
+                println("       to refresh screen");
+                println();
+                displayMessages();
+
+                String recipientName = Optional.ofNullable(recipient).map(x -> getClients().get(x).getClientName()).orElse(null);
+                if (recipientName != null) {
+                    write(Colors.YELLOW);
+                    print("[" + recipientName + "]");
+                }
+
+                write(Colors.GREY3);
+                print(">");
+                command = readLine();
+                command = defaultString(command).trim();
+                if (StringUtils.isBlank(command)) {
+                    continue;
+                } else if (command.matches("(?is)/to [a-zA-Z0-9]+")) {
+                    Long candidateRecipient = getClientIdByName(command.replaceAll("\\s+", " ").substring(4));
+                    if (candidateRecipient != null && candidateRecipient != getClientId()) {
+                        recipient = candidateRecipient;
+                    }
+                } else if (command.matches("(?is)/nick [a-zA-Z0-9]+")) {
+                    String newName = command.replaceAll("\\s+", " ").substring(6);
+                    int res = changeClientName(newName);
+                    if (res != 0) {
+                        println("Error: name already used. Press any key.");
+                        readKey();
+                    }
+                } else if (command.equalsIgnoreCase("/users")) {
+                    showUsers();
+                } else if (recipient != null) {
+                    send(recipient, new Message(recipient, command));
+                    send(getClientId(), new Message(recipient, command));
+                }
+            } while (!".".equals(command));
+        } finally {
+            changeClientName(UUID.randomUUID().toString());
+        }
     }
 
     private void showUsers() throws IOException {
