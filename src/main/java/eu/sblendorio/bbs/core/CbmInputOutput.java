4a4f65ce20dea1e028fd41f07beadcc4dd818f0a
francesco.sblendorio <francesco.sblendorio@eidosmedia.com>
2020-05-12 13:57:38 +0200

    More checks on input buffer

--- src/main/java/eu/sblendorio/bbs/core/CbmInputOutput.java
+++ src/main/java/eu/sblendorio/bbs/core/CbmInputOutput.java
@@ -477,7 +477,7 @@ public class CbmInputOutput extends Reader {
             out.close();
             this.close();
             throw new CbmIOException("SEVERE. CbmInputOutput::resetInput, potential DoS detected.");
-        } else if (missingInput.matches("(?is)^(G?ET|P?OST|H?EAD|P?UT|D?ELETE|C?ONNECT) [^\n]+ HTTP.*")) {
+        } else if (missingInput.matches("(?is)^(G?ET|P?OST|H?EAD|P?UT|D?ELETE|C?ONNECT|O?PTIONS) [^\n]+ HTTP.*")) {
             out.flush();
             out.close();
             this.close();
