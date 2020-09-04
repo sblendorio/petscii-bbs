a6d230af6cebd2f87a12d38e686f193b61dfd333
sblendorio-life <francesco.sblendorio@x10y.com>
2020-09-04 15:18:53 +0200

    Collapse spaces in post titles

--- src/main/java/eu/sblendorio/bbs/tenants/WordpressProxy.java
+++ src/main/java/eu/sblendorio/bbs/tenants/WordpressProxy.java
@@ -185,9 +185,9 @@ public class WordpressProxy extends PetsciiThread {
             Post post = new Post();
             JSONObject postJ = (JSONObject) posts.get(i);
             post.id = (Long) postJ.get("id");
-            post.content = ((String) ((JSONObject) postJ.get("content")).get("rendered")).replaceAll("(?is)(\\[/?vc_[^]]*\\])*", EMPTY);
-            post.title = (String) ((JSONObject) postJ.get("title")).get("rendered");
-            post.date = ((String) postJ.get("date")).replace("T", SPACE).replaceAll(":\\d\\d\\s*$", EMPTY);
+            post.content = defaultString((String) ((JSONObject) postJ.get("content")).get("rendered")).replaceAll("(?is)(\\[/?vc_[^]]*\\])*", EMPTY);
+            post.title = defaultString((String) ((JSONObject) postJ.get("title")).get("rendered")).replaceAll(" +", " ");
+            post.date = defaultString((String) postJ.get("date")).replace("T", SPACE).replaceAll(":\\d\\d\\s*$", EMPTY);
             post.excerpt = (String) ((JSONObject) postJ.get("excerpt")).get("rendered");
             post.authorId = toLong(postJ.get("author").toString());
             result.put(i+1+(perPage*(page-1)), post);
