92e5e5c25b68eb2c3bc01a716e78f8250c66cf8f
sblendorio <francesco.sblendorio@x10y.com>
2020-11-19 11:15:37 +0100

    add init delay

--- src/main/java/eu/sblendorio/bbs/tenants/CountDelayedTest.java
+++ src/main/java/eu/sblendorio/bbs/tenants/CountDelayedTest.java
@@ -6,8 +6,10 @@ import org.apache.commons.lang3.math.NumberUtils;
 public class CountDelayedTest extends PetsciiThread {
     @Override
     public void doLoop() throws Exception {
+        long init = NumberUtils.toLong(System.getProperty("init", "0"));
         long ms = NumberUtils.toLong(System.getProperty("delay", "2"));
         int threshold = NumberUtils.toInt(System.getProperty("threshold", "150"));
+        Thread.sleep(init);
         String s = "";
         int count = 0;
         for (int i=1; i<=276; ++i) s += " "+ i;
