17785ba74537166547da581ba2499c96b8466bd3
Francesco Sblendorio <sblendorio@gmail.com>
2023-02-13 21:33:27 +0100

    test for videotel

--- src/main/java/eu/sblendorio/bbs/tenants/minitel/TestMinitel2.java
+++ src/main/java/eu/sblendorio/bbs/tenants/minitel/TestMinitel2.java
@@ -2,13 +2,12 @@ package eu.sblendorio.bbs.tenants.minitel;
 
 import eu.sblendorio.bbs.core.MinitelThread;
 
-public class TestMinitel extends MinitelThread {
+public class TestMinitel2 extends MinitelThread {
     @Override
     public void doLoop() throws Exception {
         cls();
         println("Prova però, perché, com'è, ü, garçon");
         println("àáâäèéêëìíîïòóôöùúûüç");
         println("ÀÁÂÄÈÉÊËÌÍÎÏÒÓÔÖÙÚÛÜÇ");
-        write(readBinaryFile("minitel/undefined.vdt"));
     }
 }
