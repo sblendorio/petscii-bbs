79de93990c4dec85210c6cbdbcf4ad0d7c8f554d
Francesco Sblendorio <sblendorio@gmail.com>
2023-02-15 10:23:58 +0100

    Apulia Retrocompting

--- src/main/java/eu/sblendorio/bbs/tenants/petscii/OneApuliaRetrocomputingPetscii.java
+++ src/main/java/eu/sblendorio/bbs/tenants/petscii/OneApuliaRetrocomputingPetscii.java
@@ -1,6 +1,11 @@
 package eu.sblendorio.bbs.tenants.petscii;
 
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndEntry;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 public class OneApuliaRetrocomputingPetscii extends OneRssPetscii {
 
@@ -10,4 +15,15 @@ public class OneApuliaRetrocomputingPetscii extends OneRssPetscii {
         sections.put("1", new NewsSection("", "https://www.apuliaretrocomputing.it/wordpress/?feed=rss2"));
         LOGO_SECTION = readBinaryFile("petscii/apulia-retrocomputing.seq");
     }
+
+    @Override
+    public String getArticleBody(SyndEntry e) {
+        if (e == null || e.getContents() == null)
+            return "";
+        return e.getContents().stream()
+                .map(SyndContent::getValue)
+                .map(StringUtils::defaultString)
+                .collect(Collectors.joining("<br>"));
+    }
+
 }
