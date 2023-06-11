0c0d3ad511f5dcf6a37d7ad03673b231c950866e
Francesco Sblendorio <sblendorio@gmail.com>
2023-06-11 13:51:52 +0200

    Commute order

--- src/main/java/eu/sblendorio/bbs/tenants/ascii/OneRssAJPlusAscii.java
+++ src/main/java/eu/sblendorio/bbs/tenants/ascii/OneRssAJPlusAscii.java
@@ -2,6 +2,7 @@ package eu.sblendorio.bbs.tenants.ascii;
 
 import com.rometools.rome.feed.synd.SyndContent;
 import com.rometools.rome.feed.synd.SyndEntry;
+import eu.sblendorio.bbs.tenants.petscii.OneRssPetscii;
 import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.StandardCharsets;
@@ -22,13 +23,13 @@ public class OneRssAJPlusAscii extends OneRssAscii {
     protected void readSections() throws Exception {
         this.sections = new LinkedHashMap<>();
         this.sections.put("1", new NewsSection("General news", "https://www.ajplus.net/stories?format=rss"));
-        this.sections.put("2", new NewsSection("Climate Crisis", "https://www.ajplus.net/stories/tag/Climate?format=rss"));
-        this.sections.put("3", new NewsSection("Culture", "https://www.ajplus.net/stories/tag/Culture?format=rss"));
-        this.sections.put("4", new NewsSection("Good Trouble", "https://www.ajplus.net/stories/tag/Good+Trouble?format=rss"));
-        this.sections.put("5", new NewsSection("(in)Justice", "https://www.ajplus.net/stories/tag/Injustice?format=rss"));
-        this.sections.put("6", new NewsSection("Pandemic", "https://www.ajplus.net/stories/tag/Pandemic?format=rss"));
+        this.sections.put("2", new NewsSection("World", "https://www.ajplus.net/stories/tag/World?format=rss"));
+        this.sections.put("3", new NewsSection("Climate Crisis", "https://www.ajplus.net/stories/tag/Climate?format=rss"));
+        this.sections.put("4", new NewsSection("Culture", "https://www.ajplus.net/stories/tag/Culture?format=rss"));
+        this.sections.put("5", new NewsSection("Good Trouble", "https://www.ajplus.net/stories/tag/Good+Trouble?format=rss"));
+        this.sections.put("6", new NewsSection("(in)Justice", "https://www.ajplus.net/stories/tag/Injustice?format=rss"));
         this.sections.put("7", new NewsSection("Solutions", "https://www.ajplus.net/stories/tag/Solutions?format=rss"));
-        this.sections.put("8", new NewsSection("World", "https://www.ajplus.net/stories/tag/World?format=rss"));
+        this.sections.put("8", new NewsSection("Pandemic", "https://www.ajplus.net/stories/tag/Pandemic?format=rss"));
         this.LOGO_MENU = "Al Jazeera".getBytes(StandardCharsets.ISO_8859_1);
         this.LOGO_SECTION = (getScreenColumns() > 30 ? "Al Jazeera" : "AJ+").getBytes(StandardCharsets.ISO_8859_1);
     }
--- src/main/java/eu/sblendorio/bbs/tenants/petscii/OneRssAJPlusPetscii.java
+++ src/main/java/eu/sblendorio/bbs/tenants/petscii/OneRssAJPlusPetscii.java
@@ -13,13 +13,13 @@ public class OneRssAJPlusPetscii extends OneRssPetscii {
     protected void readSections() throws Exception {
         this.sections = new LinkedHashMap<>();
         this.sections.put("1", new NewsSection("General news", "https://www.ajplus.net/stories?format=rss"));
-        this.sections.put("2", new NewsSection("Climate Crisis", "https://www.ajplus.net/stories/tag/Climate?format=rss"));
-        this.sections.put("3", new NewsSection("Culture", "https://www.ajplus.net/stories/tag/Culture?format=rss"));
-        this.sections.put("4", new NewsSection("Good Trouble", "https://www.ajplus.net/stories/tag/Good+Trouble?format=rss"));
-        this.sections.put("5", new NewsSection("(in)Justice", "https://www.ajplus.net/stories/tag/Injustice?format=rss"));
-        this.sections.put("6", new NewsSection("Pandemic", "https://www.ajplus.net/stories/tag/Pandemic?format=rss"));
+        this.sections.put("2", new NewsSection("World", "https://www.ajplus.net/stories/tag/World?format=rss"));
+        this.sections.put("3", new NewsSection("Climate Crisis", "https://www.ajplus.net/stories/tag/Climate?format=rss"));
+        this.sections.put("4", new NewsSection("Culture", "https://www.ajplus.net/stories/tag/Culture?format=rss"));
+        this.sections.put("5", new NewsSection("Good Trouble", "https://www.ajplus.net/stories/tag/Good+Trouble?format=rss"));
+        this.sections.put("6", new NewsSection("(in)Justice", "https://www.ajplus.net/stories/tag/Injustice?format=rss"));
         this.sections.put("7", new NewsSection("Solutions", "https://www.ajplus.net/stories/tag/Solutions?format=rss"));
-        this.sections.put("8", new NewsSection("World", "https://www.ajplus.net/stories/tag/World?format=rss"));
+        this.sections.put("8", new NewsSection("Pandemic", "https://www.ajplus.net/stories/tag/Pandemic?format=rss"));
         this.screenRows = 19;
         this.pageSize = 10;
         this.showAuthor = true;
@@ -28,7 +28,6 @@ public class OneRssAJPlusPetscii extends OneRssPetscii {
         this.offsetY = 1;
         this.twoColumns = false;
         this.LOGO_MENU = this.LOGO_SECTION = readBinaryFile("petscii/aj.seq");
-
     }
 
     @Override
