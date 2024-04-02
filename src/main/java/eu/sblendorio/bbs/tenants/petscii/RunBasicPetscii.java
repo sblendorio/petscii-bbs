4f92102805df8a242be9c8179b281735101f1fe3
sblendorio@gmail.com <sblendorio@gmail.com>
2024-04-02 18:22:39 +0200

    startrek2 apple-1

--- src/main/java/eu/sblendorio/bbs/tenants/petscii/RunBasicPetscii.java
+++ src/main/java/eu/sblendorio/bbs/tenants/petscii/RunBasicPetscii.java
@@ -8,7 +8,7 @@ public class RunBasicPetscii extends PetsciiThread {
     private String source = null;
 
     public RunBasicPetscii() {
-        source = "basic/startrek-40-1.bas";
+        source = "basic/startrek-40-2.bas";
     }
 
     public RunBasicPetscii(String source) {
